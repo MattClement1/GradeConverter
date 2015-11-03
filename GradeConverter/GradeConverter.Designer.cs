786b234734f09744489d2cdacc820f978dac205b
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-11-03 14:26:22 -0500

    Grade Converter

---
+++ GradeConverter/GradeConverter.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace GradeConverter
+{
+    partial class frmGradeConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtGradeInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(32, 23);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(134, 16);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter the letter grade:";
+            // 
+            // txtGradeInput
+            // 
+            this.txtGradeInput.Location = new System.Drawing.Point(49, 51);
+            this.txtGradeInput.Name = "txtGradeInput";
+            this.txtGradeInput.Size = new System.Drawing.Size(100, 20);
+            this.txtGradeInput.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(58, 77);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.Location = new System.Drawing.Point(20, 144);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(159, 16);
+            this.lblAnswerText.TabIndex = 3;
+            this.lblAnswerText.Text = "The percentage grade is:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(87, 173);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 16);
+            this.lblAnswer.TabIndex = 4;
+            // 
+            // frmGradeConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 212);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtGradeInput);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmGradeConverter";
+            this.Text = "Grade Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtGradeInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ GradeConverter/GradeConverter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeConverter
+{
+    public partial class frmGradeConverter : Form
+    {
+        private int CalculateGrade(string grade)
+        {
+            int percentGrade= 0;
+
+            if (grade == "NE")
+            {
+                percentGrade = 0;
+            }
+            else if (grade == "R")
+            {
+                percentGrade = 30;
+            }
+            else if (grade == "1-")
+            {
+                percentGrade = 52;
+            }
+            else if (grade == "1")
+            {
+                percentGrade = 55;
+            }
+            else if (grade == "1+")
+            {
+                percentGrade = 57;
+            }
+            else if (grade == "2-")
+            {
+                percentGrade = 62;
+            }
+            else if (grade == "2")
+            {
+                percentGrade = 65;
+            }
+            else if (grade == "2+")
+            {
+                percentGrade = 67;
+            }
+            else if (grade == "3-")
+            {
+                percentGrade = 72;
+            }
+            else if (grade =="3")
+            {
+                percentGrade = 75;
+            }
+            else if (grade == "3+")
+            {
+                percentGrade = 77;
+            }
+            else if (grade == "4-")
+            {
+                percentGrade = 85;
+            }
+            else if (grade == "4")
+            {
+                percentGrade = 90;
+            }
+            else if (grade == "4+")
+            {
+                percentGrade = 95;
+            }
+            else
+            {
+                percentGrade = -1;
+            }
+
+            return percentGrade;
+        }
+
+        public frmGradeConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string gradeInput = this.txtGradeInput.Text;
+            int percentGrade;
+
+            percentGrade = CalculateGrade(gradeInput);
+
+            this.lblAnswer.Text = Convert.ToString(percentGrade);
+        }
+    }
+}
